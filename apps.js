366c924b4da96e59673aa947aa77ee977e4e8097
YOUR NAME <YOUR EMAIL>
2022-02-13 17:01:26 +0200

    n

---
+++ apps.js
@@ -0,0 +1,24 @@
+var yourname=prompt("your name is ");
+var gender=prompt("male or female ");
+var age=prompt ( "your age is " ) ;
+
+if ( age <= 0 ) {
+    alert ( "age is not allowed " )
+}  
+
+var welcome= confirm("do you want to skip welcome " ) 
+if (welcome == false);
+{
+    if ( gender == "male" ) {
+        alert( 'hello mr '+ yourname )
+    
+    }
+    else if (gender == "female" ) {
+        alert ('hello ms' + yourname)
+    }
+    else {
+        confirm ( "welcome " )
+    }
+
+
+}
